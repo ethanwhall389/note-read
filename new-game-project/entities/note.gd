80f7977c0bba25b67854742ad1903dd24ef8892e
ethanwhall389 <ethanwhall@icloud.com>
2026-03-16 17:04:12 -0400

    add simple movement to notes

--- new-game-project/entities/note.gd
+++ new-game-project/entities/note.gd
@@ -1,4 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
 # func setup(note :Dictionary) -> void:
+
+const SPEED = 100.0
+
+func _physics_process(delta: float) -> void:
+	velocity.x = -SPEED
+	move_and_slide()
 	
